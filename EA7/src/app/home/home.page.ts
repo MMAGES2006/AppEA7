6f3f3b4a0183539658e45483346ddc06ae5c8165
MMAGES2006 <22170@portalcedi.edu.mx>
2025-01-21 10:55:36 -0600

    EA casi terminada

--- EA7/src/app/home/home.page.ts
+++ EA7/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,6 +8,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  items: {
+    id: number; name: string; date: string,
+    completed: boolean
+  }[] = [];
+  nextId: number = 1;
 
+  constructor(public navCtrl: NavController) { }
+
+  addItem(name: string, date: string): void 
+  {
+    if (name.trim() && date.trim()) {
+      this.items.push({
+        id: this.nextId++,
+        name: name.trim(),
+        date: date.trim(),
+        completed: false,
+      });
+    } else {
+      console.error('El nombre y la fecha no pueden estar vacios');
+    }
+  }
+
+  toggleCompleted(id: number): void
+  {
+    const item = this.items.find(item => item.id === id);
+    if(item)
+    {
+      item.completed = !item.completed;
+    }
+  }
+
+  removeItem(id: number): void
+  {
+    this.items = this.items.filter(item => item.id !==id);
+  }
 }
+
+
